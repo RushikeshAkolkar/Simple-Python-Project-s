f413f1afcb5839e3da296c23089c36255188abe9
Rushikesh <rushikeshakolkar05@gmail.com>
2024-01-03 10:23:23 +0530

    HappyNewYear2024.py File is updated

---
+++ Email Validation/EmailValidationUsingRegex.py
@@ -0,0 +1,7 @@
+import re
+email = input("Enter You Mail Id : ")
+email_condition = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}"
+if re.search(email_condition, email):
+    print("Right Email.")
+else:
+    print("Wrong Email.")
--- QR code genrator/qr code.py
+++ QR code genrator/qr code.py
@@ -3,9 +3,3 @@ import qrcode as qr
 
 img = qr.make("www.google.com")
 img.save("google.png")
-
-#----------------------------My Phone pay qr code genrator----------------
-import qrcode as qr
-
-img = qr.make("8888440539@ybl")
-img.save("Rushikesh_Akolkar_Phone_pay_QR_Code_genrator.png")
